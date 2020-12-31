ef8ea1fb95adbec9f56895890bfab164598ea5c1
Jacob Wan <jacobwan840@gmail.com>
2020-12-31 15:46:42 -0800

    Add test for multiple consonant replacement
    Includes refactoring

--- Soundex.h
+++ Soundex.h
@@ -9,23 +9,32 @@ class Soundex
 public:
     std::string encode(const std::string &word) const
     {
-        return zeroPad(head(word) + encodedDigits(word));
+        return zeroPad(head(word) + encodedDigits(tail(word)));
     }
 
 private:
     static const size_t MaxCodeLength{4};
+
     std::string head(const std::string &word) const
     {
         return word.substr(0, 1);
     }
+
+    std::string tail(const std::string &word) const
+    {
+        return word.substr(1);
+    }
+
     std::string encodedDigits(const std::string &word) const
     {
-        if (word.length() > 1)
+        std::string encoding;
+        for (auto letter : word)
         {
-            return encodedDigit(word[1]);
+            encoding += encodedDigit(letter);
         }
-        return "";
+        return encoding;
     }
+
     std::string encodedDigit(char letter) const
     {
         const std::unordered_map<char, std::string> encodings{
@@ -50,6 +59,7 @@ private:
         auto it = encodings.find(letter);
         return it == encodings.end() ? "" : it->second;
     }
+
     std::string zeroPad(const std::string &word) const
     {
         auto zerosNeeded = MaxCodeLength - word.length();
--- SoundexTest.cpp
+++ SoundexTest.cpp
@@ -28,3 +28,8 @@ TEST_F(SoundexEncoding, IgnoresNonAlphabetics)
 {
    ASSERT_THAT(soundex.encode("A#"), Eq("A000"));
 }
+
+TEST_F(SoundexEncoding, ReplacesMultipleConsonantsWithDigits)
+{
+   ASSERT_THAT(soundex.encode("Acdl"), Eq("A234"));
+}
