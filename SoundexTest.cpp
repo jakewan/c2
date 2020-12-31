1475f0ea8e9716f6d7a77b208ad4af2ef35b84b3
Jacob Wan <jacobwan840@gmail.com>
2020-12-31 09:26:43 -0800

    Handle non-alphabetic characters

--- SoundexTest.cpp
+++ SoundexTest.cpp
@@ -23,3 +23,8 @@ TEST_F(SoundexEncoding, ReplacesConsonantsWithAppropriateDigits)
 {
    ASSERT_THAT(soundex.encode("Ax"), Eq("A200"));
 }
+
+TEST_F(SoundexEncoding, IgnoresNonAlphabetics)
+{
+   ASSERT_THAT(soundex.encode("A#"), Eq("A000"));
+}
