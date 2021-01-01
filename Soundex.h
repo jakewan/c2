280be9438f3841816d991d94787760925b49adc3
Jacob Wan <jacobwan840@gmail.com>
2020-12-31 19:36:43 -0800

    Refactor, more declarative

--- Soundex.h
+++ Soundex.h
@@ -30,13 +30,18 @@ private:
         std::string encoding;
         for (auto letter : word)
         {
-            if (encoding.length() == MaxCodeLength - 1)
+            if (isComplete(encoding))
             {
                 break;
             }
             encoding += encodedDigit(letter);
         }
         return encoding;
+    }
+
+    bool isComplete(const std::string &encoding) const
+    {
+        return encoding.length() == MaxCodeLength - 1;
     }
 
     std::string encodedDigit(char letter) const
